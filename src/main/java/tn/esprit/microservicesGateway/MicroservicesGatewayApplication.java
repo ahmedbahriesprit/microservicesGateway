7e152801ab5c50811419ef357e56c6b685e0b4f9
ahmedbahriesprit <bahri.ahmed@esprit.tn>
2023-04-30 21:52:46 +0100

    update gateway

--- src/main/java/tn/esprit/microservicesGateway/MicroservicesGatewayApplication.java
+++ src/main/java/tn/esprit/microservicesGateway/MicroservicesGatewayApplication.java
@@ -2,7 +2,9 @@ package tn.esprit.microservicesGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicesGatewayApplication {
 
